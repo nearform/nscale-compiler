b26daca761ed800a1a7fe361c00a965ba68831e0
Peter Elger <elger.peter@gmail.com>
2014-11-14 08:30:34 +0000

    check for elements with no matching definition

---
+++ test/fixture/missing/system.js
@@ -0,0 +1,33 @@
+// Define the system topology here. The topolgy should reference 
+// containers defined in definitions/services.js
+
+exports.name = 'isnc';
+exports.namespace = 'isnc';
+exports.id = '12121212-bf98-4d29-b1c8-d1885bc5c294';
+
+exports.topology = {
+  local: {
+    root: [
+      'postgres',
+      'redis',
+      'elasticsearch',
+      'consul-onemachine',
+      'users',
+      'permissions',
+      'business-logic',
+      'audit',
+      'emails',
+      'frontend'
+    ]
+  },
+
+  direct: {
+    machine$a: {contains: ['frontend'],
+                  specific: { ipAddress: '1.2.3.4'}},
+    fish: {contains: ['users', 'permissions', 'business-logic', 'audit', 'emails'],
+                  specific: { ipAddress: '10.20.30.40'}},
+    machine$fe2: {contains: ['frontend'],
+                  specific: { ipAddress: '5.6.7.8'}}
+  }
+};
+
